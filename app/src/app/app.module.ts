0220d78739cce19dc00162dfe3a27b4e8ba39639
李 晟在 <sungjae.lee@jeune.abc-cooking.co.jp>
2020-02-15 09:43:36 +0900

    メニューのファイルパスを再構築

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { MenuComponent } from './index/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
