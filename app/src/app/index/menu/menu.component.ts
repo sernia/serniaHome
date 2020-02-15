af9ce8a805cabb63b05f1e05a5301e3f961c1fca
李 晟在 <sungjae.lee@jeune.abc-cooking.co.jp>
2020-02-15 17:48:30 +0900

    メイン画面を設計

--- app/src/app/index/menu/menu.component.ts
+++ app/src/app/index/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +10,14 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+      // visible height + pixel scrolled >= total height
+      if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+        console.log("End");
+      }
   }
 
 }
